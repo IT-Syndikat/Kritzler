ad0714271fc8c0a1f7c3cd6714f958cf4258c67c
robelix <roland@robelix.com>
2018-10-18 02:51:26 +0200

    java8

--- plotter.sh
+++ plotter.sh
@@ -1,4 +1,4 @@
 
 export CP=/usr/share/java/rxtx/RXTXcomm.jar:lib/RXTXcomm.jar:lib/core.jar:lib/serial.jar:bin:lib/geomerative.jar:lib/controlP5.jar
 
-/usr/lib/jvm/java-7-openjdk/jre/bin/java -Djava.library.path=/usr/lib -Dfile.encoding=UTF-8  -classpath $CP com.tinkerlog.kritzler.Plotter
+/usr/lib/jvm/java-8-openjdk/jre/bin/java -Djava.library.path=/usr/lib -Dfile.encoding=UTF-8  -classpath $CP com.tinkerlog.kritzler.Plotter
